08c5e3e4fc0692efa020e4acf8e54d7b44780ad7
Franco Cyberpunx <franco.mgr@gmail.com>
2021-01-28 14:43:33 -0300

    script start gunicorn

---
+++ scripts/start_gunicorn.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+NAME="$1" # Name of the application
+DJANGODIR=/root/admincar/admincar # Django project directory
+SOCKFILE=/run/gunicorn_admincar.sock # we will communicte using this unix socket
+USER=root # the user to run as
+GROUP=root # the group to run as
+NUM_WORKERS=1 # how many worker processes should Gunicorn spawn
+DJANGO_SETTINGS_MODULE=admincar.settings # which settings file should Django use
+DJANGO_WSGI_MODULE=admincar.wsgi # WSGI module name
+
+echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+cd $DJANGODIR
+source ~/venv/admincar/bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+cd ~
+exec venv/admincar/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER --group=$GROUP \
+  --bind=unix:$SOCKFILE \
+  --log-level=debug \
+  --log-file=-
