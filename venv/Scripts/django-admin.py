047be8fb9e52dc82ceaaeb0882fe81607613d56f
Cyberpunx <franco.mgr@gmail.com>
2021-01-28 21:00:30 -0300

    VENV update

--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!C:\Users\Fenrir\PycharmProjects\crispy-eurekaVenv\Scripts\python.exe
+#!C:\Users\Fenrir\PycharmProjects\crispy-eureka\venv\Scripts\python.exe
 from django.core import management
 
 if __name__ == "__main__":
